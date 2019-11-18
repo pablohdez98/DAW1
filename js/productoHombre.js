aec531cb6fcc157626f15956bd1824c79e7e95e2
Pablo Hernández Carrascosa <56001836+pablohdez98@users.noreply.github.com>
2019-11-18 23:09:46 +0000

    Update productoHombre.js

--- js/productoHombre.js
+++ js/productoHombre.js
@@ -8,7 +8,7 @@ function loadJSON() {
         $('#product').append('\
         <div class="col-lg-4 col-md-6 mb-4">\
           <div class="card h-100">\
-            <a href="../product.html?id=' + item.id + '"><img class="card-img-top" src="' + item.img + '" alt=""></a>\
+            <a href="../product.html?id=' + item.id + '"><img class="card-img-top" src="/' + item.img + '" alt=""></a>\
             <div class="card-body">\
               <p class="card-title"><a href="../product.html?id=' + item.id + '" class="text-secondary h5">' + item.name + '</a></p>\
               <p class="h4">' + item.price + '</p>\
